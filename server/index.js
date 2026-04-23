baa314769c7659051c707f9153a0d28f019186e8
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2026-04-23 17:43:57 +0100

    refactor: move database connection logic to config file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import projectRoutes from './src/routes/projects.routes';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./src/config/db";
+import projectRoutes from "./src/routes/projects.routes";
 
 dotenv.config();
+
+// DB Connection
+connectDB();
 
 const app = express();
 
@@ -13,15 +16,5 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use('/api/projects', projectRoutes);
+app.use("/api/projects", projectRoutes);
 
-// Database Connection
-const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
-
-mongoose.connect(MONGO_URI)
-  .then(() => {
-    console.log('✅ Connected to MongoDB Atlas');
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-  })
-  .catch((err) => console.error('❌ MongoDB Connection Error:', err));
